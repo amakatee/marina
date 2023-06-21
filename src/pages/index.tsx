a98a512eb41eb5a8055c86d5cf18984017ead192
Ekaterina Zinina <ekaterinazinina@Ekaterinas-MacBook-Pro.local>
2023-06-21 19:26:08 +0300

    new

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -70,7 +70,7 @@ function AuthShowcase() {
       </p>
       <button
         className="rounded-full bg-white/10 px-10 py-3 font-semibold text-white no-underline transition hover:bg-white/20"
-        onClick={sessionData ? async () =>  await signOut() : async () => await signIn()}
+        onClick={sessionData ? () => void signOut() : () => void signIn()}
       >
         {sessionData ? "Sign out" : "Sign in"}
       </button>
