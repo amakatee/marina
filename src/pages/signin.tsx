df6249a2525ed60a2f534de1618c8b7c6b31994e
Ekaterina Zinina <ekaterinazinina@Ekaterinas-MacBook-Pro.local>
2023-06-21 21:26:22 +0300

    props

--- src/pages/signin.tsx
+++ src/pages/signin.tsx
@@ -1,35 +1,25 @@
 import { getProviders, signIn, signOut, useSession } from "next-auth/react";
 import { AppProps } from "next/app";
-import Head from "next/head";
-import Link from "next/link";
-import { api } from "~/utils/api";
+
 import type { GetServerSideProps } from "next";
 
 export default function Signin({providers} : {providers: AppProps}) {
 
   return (
     <>
-    <div>Sign in with google</div>
     <div>
         {Object.values(providers).map((provider) => (
             <button
             key={provider.id}
-            onClick={() => signIn(provider.id, {
+            onClick={() => void signIn(provider.id, {
                 callbackUrl: `${window.location.origin}`
             })}
             >
-                sigin g
+                sign in with google
 
             </button>
-        ))
-
-        
-     }
+        ))}
     </div>
-    
-   
-  
-    
     </>
   );
 }
