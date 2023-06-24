5afe604731167bf01754d8d12b8e5c11a86e197b
Ekaterina Zinina <ekaterinazinina@Ekaterinas-MacBook-Pro.local>
2023-06-24 18:02:25 +0300

    product upload

--- src/components/NewProductForm.tsx
+++ src/components/NewProductForm.tsx
@@ -3,6 +3,8 @@ import { Button } from "./Button"
 import { useSession } from "next-auth/react"
 import { api } from "~/utils/api"
 import type { FormEvent } from "react"
+import {v4 } from 'uuid';
+
 function updateTextAreaSize(textArea? : HTMLTextAreaElement){
     if (textArea == null) return
     textArea.style.height = "0"
@@ -73,9 +75,102 @@ export function NewProductForm() {
         createProduct.mutate({description: inputVal, title: "duhfuash", quantity: 3, price:"600"})
     }
 
+
+    const VariationsInput = [{
+      colorValue: "",
+      sizeValue: "",
+      qtyValue: ""
+    }]
+  
+    const [variationsArr, setVariationsArr] = useState(VariationsInput)
+    
+    function addVariations() {
+      const list = [...variationsArr, { colorValue:"", sizeValue:"", qtyValue:""}]
+      setVariationsArr(list)
+    }
+    
+    function updateInputState (e: React.ChangeEvent<HTMLInputElement>, i:number) {
+       const { name, value} = e.target
+       const newArr = variationsArr.map((item, index) => {
+         if(i === index) {
+           return { ...item, [name]:value }
+         }
+         else {
+           return item
+         }
+       })
+       setVariationsArr(newArr)
+
+
+     }
+
+  
+
     return <form onSubmit={handleSubmit} className="flex flex-col gap-2 border-b py-2">
         <div className="flex flex-col gap-4 ">
             <p className="px-4">Create new product</p>
+           
+
+          
+
+            <div className="w-full bg-[pink]">
+              <p> Add Products Variations</p>
+              {variationsArr.map((item, i) => (
+                <div key={i} onClick={(e) => {
+                  e.preventDefault()
+                  console.log(variationsArr)
+                }}>
+                <input 
+                name="sizeValue"
+                placeholder="size"
+                value={item.sizeValue}
+                type="text"
+                onChange={e => updateInputState(e, i)
+              }
+
+                />
+                <input 
+                name="colorValue"
+                placeholder="color"
+                value={item.colorValue}
+                type="text"
+                onChange={e => updateInputState(e, i)}
+
+                />
+                <input 
+                name="qtyValue"
+                placeholder="qty"
+                value={item.qtyValue}
+                type="text"
+                onChange={e => updateInputState(e, i)}
+
+                />
+                 <button type="submit">Save variations</button>
+                </div>
+               
+              ))}
+              <button onClick={addVariations}>Add varioation:</button>
+              <button>Save variations</button>
+            </div>
+            
+
+            {/* <div className="flex flex-col gap-4">
+              
+              <p>Add Colors: </p>
+              <button onClick={addColorInput}>
+              Add Color:
+            </button>
+
+              {colorsArr.map((item, i) => {
+                return (
+                  <input
+                  className="bg-[orange]"
+
+                  />
+                )
+              })}
+              
+            </div> */}
             <input
             value={titleVal}
             onChange={(e) => setTitleVal(e.target.value)}
@@ -94,3 +189,6 @@ export function NewProductForm() {
 
     </form>
 }
+
+
+// https://www.cluemediator.com/add-or-remove-input-fields-dynamically-with-reactjs
